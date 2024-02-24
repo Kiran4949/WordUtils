feaecc4f42fcb41fabd38fa884930537cc4d08ce
Kiran4949 <kiranajnalkar4949@gmail.com>
2024-02-24 17:27:46 +0530

    Updated

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Alert(props) {
 
   return (
-    <div style={{height: '50px'}}>
+    <div style={{height: '50px', marginTop: '50px',}}>
     {props.alert && <div className="alert alert-dismissible fade show text-primary" role="alert" style={{color: props.alert === 'white'?'dark':'white'}}>
         <strong> {props.alert.type} </strong> {props.alert.msg}
     </div>}
